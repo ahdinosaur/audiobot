458d0798fae7cda0cb38df2ab3be4050bee2d7c0
Michael Williams <Sir.Michael.Williams@gmail.com>
2012-03-16 04:18:42 -0700

    moved unusable to bad branch

--- testclient/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject testclient "test client for playpausestop"
-  :description "work in progress"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.leadtune/clojure-zmq "2.1.0"]]
-  :native-dependencies [[org.clojars.starry/jzmq-native-deps "2.0.10.4"]]
-  :dev-dependencies [[native-deps "1.0.1"]]
-  :main testclient.core)
